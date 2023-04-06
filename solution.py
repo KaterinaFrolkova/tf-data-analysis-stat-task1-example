59c028a1ba7877457acbe3154f0230c6da586eb0
KaterinaFrolkova <frolkova.ek@gmail.com>
2023-04-06 18:06:40 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 374341160 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return (x[-1] - x[0])/((len(x) - 1) * 10) # Ваш ответ
